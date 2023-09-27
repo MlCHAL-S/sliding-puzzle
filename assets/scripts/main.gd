52d227c59377f4eb138e3c82ff622cd7a79e471a
Michal Szynkaruk <michal.szynkaruk@wp.pl>
2023-09-27 12:32:33 +0200

    Changed timer in main to 2

--- assets/scripts/main.gd
+++ assets/scripts/main.gd
@@ -5,7 +5,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 #	get_tree().get_current_scene().guardedDelayedChangeSceneWithPath("res://assets/scenes/menu_screen.tscn")
-	yield(get_tree().create_timer(0.5), "timeout")
+	yield(get_tree().create_timer(2), "timeout")
 	get_tree().current_scene.changeSceneWithFilePath("res://assets/scenes/menu_screen.tscn")
 	
 
